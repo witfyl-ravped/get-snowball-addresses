7e4099905ac6921b12a7911244136779cee1754a
witfyl-ravped <witfyl-ravped@gmail.com>
2021-05-10 22:28:22 -0500

    successfully pulling events, but only one at a time due to block limit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import abi from './abi/abi.json'
+import { ethers } from 'ethers';
+
+const getEvents = async () => {
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+  let logInfo = {
+    address: '0x60B1A1EB0374861FE79CE946726dB1ffe2b6eC54',
+    fromBlock: 1654739,
+    toBlock: 1654739
+  }
+
+  provider.getLogs(logInfo).then((res) => console.log(res))
+}
 
 function App() {
+  getEvents();
   return (
     <div className="App">
       <header className="App-header">
@@ -23,3 +39,13 @@ function App() {
 }
 
 export default App;
+
+  // const contractAddress = "0x60B1A1EB0374861FE79CE946726dB1ffe2b6eC54";
+
+  // const contract = new ethers.Contract(contractAddress, abi, provider.getSigner(0));
+  // let eventFilter = contract.filters.Deposit();
+  // let events = await contract.queryFilter(eventFilter, -100);
+
+  // console.log(events);  
+  // console.log(contract);
+  // await contract.getTransactionCount(true, true).then(count => console.log(count.toString()));
